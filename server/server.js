04f7f3f7280ba735f2203a05aba3cd9802874358
Xforzan <nakhila2024@gmail.com>
2025-10-05 15:24:06 +0200

    .

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const { connectDB } = require("./src/config/db");
@@ -17,11 +16,6 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
-app.use(cors({
-  origin: true,  // permite cualquier origen
-  credentials: true
-}));
 
 app.use(cookieParser());
 
